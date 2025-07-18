ef84f157a1c92a21b65014fcd39eca5bd309c0b5
hangsheng0625 <hangsheng279@gmail.com>
2025-07-18 22:28:03 +0800

    create admin folder and set up basic admin stuff

---
+++ admin/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JS/JSX files in src
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
