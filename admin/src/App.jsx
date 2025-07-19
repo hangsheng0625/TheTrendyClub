ee74be2ceb6c658f00252e98bf1360fdf717a9b1
hangsheng0625 <hangsheng279@gmail.com>
2025-07-19 22:20:19 +0800

    Complete the route setting for list, add and orders in admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import {Routes, Route} from 'react-router-dom'
+import Add from './pages/Add'
+import Orders from './pages/Orders'
+import List from './pages/List'
+
 
 const App = () => {
   return (
@@ -10,6 +15,13 @@ const App = () => {
       <hr />
       <div className='flex w-full '>
         <Sidebar/>
+        <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base'>
+          <Routes>
+            <Route path='/add' element={<Add></Add>}/>
+            <Route path='/list' element={<List></List>}/>
+            <Route path='/orders' element={<Orders></Orders>}/>
+          </Routes>
+        </div>
 
       </div>
       </>
