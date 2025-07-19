a2a08a282b28c64528c6bfb255685eacd1cde48a
hangsheng0625 <hangsheng279@gmail.com>
2025-07-19 22:10:48 +0800

    modify the navbar and sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
-    <div>
-      <Navbar>
+    <div className='bg-gray-50 min-h-screen'>
+      <>
+      <Navbar/>
+      <hr />
+      <div className='flex w-full '>
+        <Sidebar/>
 
-      </Navbar>
+      </div>
+      </>
     </div>
+    
   )
 }
 
