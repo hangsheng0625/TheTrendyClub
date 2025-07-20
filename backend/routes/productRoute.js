620047732a922c0c2ee197436c74578ea9a4266d
hangsheng0625 <hangsheng279@gmail.com>
2025-07-20 22:56:04 +0800

    create the list page in admin and modify the productRoute from backend

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,8 +6,8 @@ import adminAuth from '../middleware/adminAuth.js';
 const productRouter = express.Router();
 
 productRouter.post('/add', adminAuth, upload.fields([{name:'image1', maxCount:1}, {name:'image2', maxCount:1}, {name:'image3', maxCount:1}, {name:'image4', maxCount:1}]), addProduct);
-productRouter.post('/list', adminAuth, listProduct);
-productRouter.post('/remove', removeProduct);
+productRouter.get('/list', listProduct);
+productRouter.post('/remove', adminAuth, removeProduct);
 productRouter.post('/single', singleProduct);
 
 export default productRouter;
